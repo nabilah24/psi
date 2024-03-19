500692ca8bd3f47404d00635f2cc6a0389c21484
nabilah24 <nabilah.zakiyah.2205356@students.um.ac.id>
2024-03-20 06:36:46 +0700

    commit

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <title>Register</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="{{ asset('assets/css/styles1.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 </head>
 
